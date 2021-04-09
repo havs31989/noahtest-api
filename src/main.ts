82c1b36a217385587a4f08ac664f4bbd57055016
Vo Son Ha <vosonha89@outlook.com>
2021-04-09 13:42:26 +0700

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,21 @@ import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import configuration from '../config/configuration';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const port = configuration().port;
-  console.log('server listen at ' + port);
-  await app.listen(configuration().port);
+    const app = await NestFactory.create(AppModule);
+    if (process.env.ENV == 'DEV') {
+        const config = new DocumentBuilder()
+            .setTitle('NoahTest API')
+            .setDescription('NoahTest API description')
+            .setVersion('1.0')
+            .build();
+        const document = SwaggerModule.createDocument(app, config);
+        SwaggerModule.setup('docs', app, document);
+    }
+    const port = configuration().port;
+    console.log('server listen at ' + port);
+    await app.listen(configuration().port);
 }
 bootstrap();
